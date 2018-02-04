03ff81694c89f4127566db12e95bc15733c5c6bd
Alberto Carreras <acarrerasc@gmail.com>
2018-02-04 22:53:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,15 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = new Array();
+  for (let i = 0 ;i < musicians.length ; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  while(facts.length > 0) {
+    facts[facts.length] = `${facts[facts.length]}!!!`
+    --facts.length
+  }
+  return facts
+}
